082fb825440b34feb1767fd917e454e0d597789e
Scarlett Baez <s.baez1010@gmail.com>
2020-05-31 22:17:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 // write your code below!
-function happyHolidays(); {
-return('Happy Holidays!');
+function happyHolidays() {
+return('Happy Holidays!')
 }
 
-function happyHolidaysTo('Caroline'); {
-return(`Happy Holidays, ${name}!`);
+function happyHolidaysTo('Caroline') {
+return(`Happy Holidays, ${name}!`)
 }
 
-function happyCustomHolidayTo('Chirstmas, Caroline!'); {
-return(`Happy ${holiday}, ${name}!`);
+function happyCustomHolidayTo('Chirstmas, Caroline!') {
+return(`Happy ${holiday}, ${name}!`)
 }
 
-function holidayCountdown('Chirstmas, 209'); {
-return(`It's ${days} days until ${holiday}!`);
+function holidayCountdown('Chirstmas, 209') {
+return(`It's ${days} days until ${holiday}!`)
 }
