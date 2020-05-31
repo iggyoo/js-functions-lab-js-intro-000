1615d9c648e55a256923d2189a26183041b65c99
Scarlett Baez <s.baez1010@gmail.com>
2020-05-31 22:18:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 // write your code below!
 function happyHolidays() {
-return('Happy Holidays!')
+return 'Happy Holidays!'
 }
 
 function happyHolidaysTo('Caroline') {
-return(`Happy Holidays, ${name}!`)
+return `Happy Holidays, ${name}!`
 }
 
 function happyCustomHolidayTo('Chirstmas, Caroline!') {
-return(`Happy ${holiday}, ${name}!`)
+return `Happy ${holiday}, ${name}!`
 }
 
 function holidayCountdown('Chirstmas, 209') {
-return(`It's ${days} days until ${holiday}!`)
+return `It's ${days} days until ${holiday}!`
 }
